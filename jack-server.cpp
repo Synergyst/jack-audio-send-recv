c3e5152902fb1f05fec91129a4324160b6b8e6f7
Synergyst <coletonm97@gmail.com>
2023-09-10 17:52:09 -0500

    Update jack-server.cpp

--- jack-server.cpp
+++ jack-server.cpp
@@ -3,6 +3,9 @@
 #include <jack/jack.h>
 #include <boost/asio.hpp>
 #include <chrono>
+int curPort = 19977;
+const int startingPort = curPort;
+std::string curClient = "AudioServer";
 constexpr int EXPECTED_AUDIO_BUFFER_SIZE = 1024 * sizeof(float);
 template <typename T1, typename T2, typename T3>
 class AudioServer {
@@ -10,7 +13,7 @@ public:
     AudioServer(T1 port, T2 jackServerName, T3 socket) : port(port), jackServerName(jackServerName), socket(socket) { }
     void run() {
         if (InitializeJack() != 0) {
-            std::cerr << "Failed to initialize JACK" << std::endl;
+            fprintf(stderr, "Failed to initialize JACK\n");
             return;
         }
     }
@@ -32,37 +35,37 @@ private:
         jack_status_t status;
         client = jack_client_open(jackServerName.c_str(), JackNullOption, &status);
         if (client == nullptr) {
-            std::cerr << "Failed to open JACK client" << std::endl;
+            fprintf(stderr, "Failed to open JACK client\n");
             return -1;
         }
         // Register JACK ports and set the process callback.
         inputPort = jack_port_register(client, "in", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
         outputPort = jack_port_register(client, "out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
         if (jack_set_process_callback(client, AudioCallback, this) != 0) {
-            std::cerr << "Failed to set JACK process callback for client" << std::endl;
+            fprintf(stderr, "Failed to set JACK process callback for client\n");
             jack_client_close(client);
             return -1;
         }
         if (jack_activate(client) != 0) {
-            std::cerr << "Failed to activate JACK for client" << std::endl;
+            fprintf(stderr, "Failed to activate JACK for client\n");
             jack_client_close(client);
             return -1;
         }
         // Automatically connect ports (add your own connections).
         jack_connect(client, "at2020_input:capture_1", jack_port_name(inputPort));
         jack_connect(client, jack_port_name(outputPort), "assound1_output:playback_1");
-        std::cout << "JACK audio backend initialized, running now." << std::endl;
+        fprintf(stderr, "JACK audio backend initialized, running now.\n");
         return 0;
     }
     static int AudioCallback(jack_nframes_t nframes, void *arg) {
         AudioServer* audioServer = static_cast<AudioServer*>(arg);
-        boost::asio::ip::tcp::socket *socket = audioServer->socket;
+        //boost::asio::ip::tcp::socket *socket = audioServer->socket;
         jack_default_audio_sample_t* in = (jack_default_audio_sample_t*)jack_port_get_buffer(audioServer->inputPort, nframes);
         jack_default_audio_sample_t* out = (jack_default_audio_sample_t*)jack_port_get_buffer(audioServer->outputPort, nframes);
         float out_buffer[EXPECTED_AUDIO_BUFFER_SIZE];
         size_t dataSize = nframes * sizeof(float);
         try {
-            size_t bytesSent = socket->send(boost::asio::buffer((float*)in, dataSize));
+            size_t bytesSent = audioServer->socket->send(boost::asio::buffer((float*)in, dataSize));
             if (bytesSent != EXPECTED_AUDIO_BUFFER_SIZE)
                 fprintf(stderr, "send under-run: %d\n", dataSize);
         } catch (const boost::system::system_error &e) {
@@ -72,12 +75,11 @@ private:
             } else {
                 fprintf(stderr, "Error while receiving from socket in client_in: %s\n", e.what());
                 audioServer->deinitJack();
-                //deinitJack(audioServer->client);
                 return 0;
             }
         }
         try {
-            size_t bytesRead = socket->receive(boost::asio::buffer(out_buffer, dataSize));
+            size_t bytesRead = audioServer->socket->receive(boost::asio::buffer(out_buffer, dataSize));
             std::memcpy(out, out_buffer, bytesRead);
             if (bytesRead != EXPECTED_AUDIO_BUFFER_SIZE)
                 fprintf(stderr, "recv under-run: %d\n", bytesRead);
@@ -88,7 +90,6 @@ private:
             } else {
                 fprintf(stderr, "Error while receiving from socket in client_out: %s\n", e.what());
                 audioServer->deinitJack();
-                //deinitJack(audioServer->client);
                 return 0;
             }
         }
@@ -97,42 +98,40 @@ private:
 };
 // Setup JACK audio backend and TCP server
 int TCPClientHandler(boost::asio::ip::tcp::socket socket) {
+    fprintf(stderr, "Client connected (%s:%d)\n", socket.remote_endpoint().address().to_string().c_str(), socket.remote_endpoint().port());
+    AudioServer<int, std::string, boost::asio::ip::tcp::socket*> obj1(curPort, curClient + "_" + std::to_string(curPort), &socket);
+    obj1.run();
+    curPort++;
     try {
         while (true) {
-            std::vector<char> buf(1);
+            float buffer[EXPECTED_AUDIO_BUFFER_SIZE];
+            // Read from the socket without clearing the data
+            boost::asio::mutable_buffers_1 customBuffer = boost::asio::buffer(buffer, EXPECTED_AUDIO_BUFFER_SIZE);
             boost::system::error_code ec;
             // Check if the client has disconnected
-            size_t bytesRead = socket.read_some(boost::asio::buffer(buf), ec);
+            size_t bytesRead = boost::asio::read(socket, customBuffer, ec);
             if (ec == boost::asio::error::eof) {
                 fprintf(stderr, "Client disconnected (%s:%d)\n", socket.remote_endpoint().address().to_string().c_str(), socket.remote_endpoint().port());
                 break;
             } else if (ec) {
-                std::cerr << "Error during data receive: " << ec.message() << std::endl;
+                fprintf(stderr, "Error during data receive: %s\n", ec.message());
                 break;
             }
             // Handle incoming data from the client here. You can use socket.read_some() or other async read operations. For simplicity, we just sleep for a short time in this example.
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            std::this_thread::sleep_for(std::chrono::milliseconds(120000));
         }
     } catch (const std::exception& e) {
-        std::cerr << "Exception in client handler: " << e.what() << std::endl;
+        fprintf(stderr, "Exception in client handler: %s\n", e.what());
     }
     return 0;
 }
 int main() {
-    int curPort = 19977;
-    const int startingPort = curPort;
-    std::string curClient = "AudioServer";
     boost::asio::io_service ioService;
     boost::asio::ip::tcp::acceptor acceptor(ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), startingPort));
     boost::asio::ip::tcp::socket socket(ioService);
     while (true) {
-        //boost::asio::ip::tcp::socket socket(ioService);
         fprintf(stderr, "Awaiting for new client on: 0.0.0.0:%d\n", startingPort);
         acceptor.accept(socket);
-        fprintf(stderr, "Client connected (%s:%d)\n", socket.remote_endpoint().address().to_string().c_str(), socket.remote_endpoint().port());
-        AudioServer<int, std::string, boost::asio::ip::tcp::socket*> obj1(curPort, curClient + "_" + std::to_string(curPort), &socket);
-        obj1.run();
-        curPort++;
         std::thread(TCPClientHandler, std::move(socket)).detach();
     }
     std::this_thread::sleep_for(std::chrono::seconds(20));
